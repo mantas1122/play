dd5a9ee91cbaee97de0b56a8453401070260b534
mantas1122 <mantas.stanionis@gmail.com>
2019-11-17 16:15:01 +0200

    test1

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Link from 'next/link'
+import Link from "next/link";
 export default () => (
   <div>
-    Hello World.{' '}
-    <Link href='/about'>
-      <a>About</a>
+    Hello World. 1111ddd1{" "}
+    <Link href="/about">
+      <a>About aaaaa</a>
     </Link>
   </div>
-)
+);
