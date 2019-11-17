a78b6be1d950dc17dff4cb3faefcf3dca76d28f6
mantas1122 <mantas.stanionis@gmail.com>
2019-11-17 16:43:16 +0200

    init
    t

--- pages/about.js
+++ pages/about.js
@@ -1,1 +1,12 @@
-export default () => <div>About us</div>
+import Layout from "../components/Layout";
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About BitzPrice</h1>
+      <p>Application to view Bitcoin prices</p>
+    </div>
+  </Layout>
+);
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
-import Link from "next/link";
-export default () => (
-  <div>
-    Hello World. 1111ddd1{" "}
-    <Link href="/about">
-      <a>About aaaaa</a>
-    </Link>
-  </div>
+import Fetch from "isomorphic-unfetch";
+import Layout from "../components/Layout";
+
+import Prices from "../components/Prices";
+
+const Index = props => (
+  <Layout>
+    <div>
+      <h1>Welcome to BitzPrice</h1>
+      <p>Check current Bitcoin rate</p>
+      <Prices bpi={props.bpi} />
+    </div>
+  </Layout>
 );
+
+Index.getInitialProps = async function() {
+  const res = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
+  const data = await res.json();
+
+  return {
+    bpi: data.bpi
+  };
+};
+
+export default Index;
